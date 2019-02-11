1ed3bbf5b5875eae0ce0467e632856d241d9a1fc
Gustavo Ribeiro <gustavors071998@gmail.com>
2019-02-11 15:22:49 -0200

    Mais detalhado

--- clock.c
+++ clock.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
-#include <math.h>
 #include <time.h>
 #include <stdlib.h>
-#include <string.h>
 
 
-int burble_sort( int arr[10000],int tam )
+void burble_sort( int arr[100000],int tam )
   {
     double time1, timedif;
+    long cont=0;
     time1 = (double) clock();
     time1 = time1 / CLOCKS_PER_SEC;
 
       int i,j,aux;
       for(i=0;i<tam;i++){
-        for(j=0;j<tam;j++){
-            if(arr[j]>arr[j+1]){
+          for(j=0;j<tam;j++){
+            if(arr[j] > arr[j+1]){
+                cont++;
                 aux = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = aux;
-            }
+         }
         }
       }
-      timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
-      printf("Tempo do burble: %.3f s\n", timedif);
-      return arr[10000];
+    timedif = ( ((double) clock()) / CLOCKS_PER_SEC) - time1;
+    printf("--------------------------\nTempo da Ordenação: %.3fs\n--------------------------\nNumero de trocas: %lu\n--------------------------\n", timedif,cont);
   }
 
-void main()
+int main()
   {
 
     int a,t,i;
-    int numeros[10000];
-    t = 10000;
+    int numeros[100000];
+    t = 1000;
     for(a=0;a<t;a++){
-        numeros[a] = rand()%100;
-        if(numeros[a]<0){
+        numeros[a] = rand()%101;
+        if(numeros[a] < 0){
             numeros[a] = rand()%100;
         }
     }
-    numeros[10000] = burble_sort(numeros,t);
+    burble_sort(numeros,t);
 
-  }
+    return 0;
+}
